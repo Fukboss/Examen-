66e001f9718baaaa42cbd8ae5a40aa9941977a14
Fukboss <marti.mateu@estudiants.urv.cat>
2023-11-17 09:00:17 +0100

    proba2

--- code.html/js.js
+++ code.html/js.js
@@ -1,26 +1,24 @@
-const URL= ' https://wizard-world-api.herokuapp.com';
+const URL= ' https://wizard-world-api.herokuapp.com/Wizards';
 
-window.onload = async()=>{
-    const pokemons = await getAllPokemon();
-  const spinnerHtmlElement = document.getElementById('spinner');
-  spinnerHtmlElement.remove();
-  for(const pokemon of pokemons){
-      const infop = await getInfoPoke(pokemon.url)
+
+window.onload = async () => {
+
+    const films = await getAllStarWarsFilms();
+  
+
+  
+    for (const film of films) {
       const mainHtmlElement = document.getElementById('main');
       const newElement = document.createElement('div');
-      
       newElement.innerHTML = `
-      <h2>${pokemon.name}</h2>
-      <p>${infop.types.map(typeContainer => typeContainer.type.name).join(', ')}</p>
-      <p>${infop.stats.map(statsname => statsname.stat.name  &&  statsname.base_stat)}</p>
+        <h2>${film}</h2>
       `;
       mainHtmlElement.appendChild(newElement);
-      
-  }
-
-};
-async function getAllPokemon(){
-    const response = await fetch(`${SWAPI_BASE_URL}/pokemon`);
+    }
+  };
+  
+  async function getAllStarWarsFilms() {
+    const response = await fetch(`${URL}/name`);
     const data = await response.json();
     return data.results;
   }
