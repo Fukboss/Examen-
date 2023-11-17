5a1762057bd8976fff2cbd15e6a7a4a3aa807d59
Fukboss <marti.mateu@estudiants.urv.cat>
2023-11-17 10:01:59 +0100

    probas

--- code.html/js.js
+++ code.html/js.js
@@ -4,6 +4,7 @@ const URL= ' https://wizard-world-api.herokuapp.com';
 window.onload = async () => {
 
     const personatjes = await getAllpersonatjes();
+    const ElixirsS= await getAllElixirs();
     //const ElixirsS= await getAllElixirs();
 
   
@@ -12,16 +13,32 @@ window.onload = async () => {
       const newElement = document.createElement('div');
       newElement.innerHTML = `
         <h2>${personatje.firstName}</h2>
-        
       `;
+      for(const Elixir of ElixirsS){
+        const mainHtmlElements = document.getElementById('main');
+      const newElements = document.createElement('div');
+      newElements.innerHTML = `
+        <h3>${personatje.firstName}</h3>
+      `;
+      }
+
       mainHtmlElement.appendChild(newElement);
+      mainHtmlElement.appendChild(newElements);
+
     }
+
    
-  };
+ 
   
   async function getAllpersonatjes() {
     const response = await fetch(`${URL}/Wizards`);
     const data = await response.json();
     return data;
   }
+  async function getAllElixirs() {
+    const response = await fetch(`${URL}/Wizards`);
+    const data = await response.json();
+    return data.elixirs;
+  }
+};
   
